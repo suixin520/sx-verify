8c0760a7654d604a38cfba0f07f6f2bdebdeee29
宋博 <songbo@songbodeMacBook-Pro.local>
2017-09-19 14:31:29 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var sx_verify = {
+  //去除空格  type 1-所有空格  2-前后空格  3-前空格 4-后空格
+  trim: function (str, type) {
+    switch (type) {
+        case 1:
+            return str.replace(/\s+/g, "");
+        case 2:
+            return str.replace(/(^\s*)|(\s*$)/g, "");
+        case 3:
+            return str.replace(/(^\s*)/g, "");
+        case 4:
+            return str.replace(/(\s*$)/g, "");
+        default:
+            return str;
+    }
+  },
+}
+
+exports.sx_verify = sx_verify;
